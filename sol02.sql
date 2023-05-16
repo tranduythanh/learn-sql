56720a0efbccd854aab9da739577d158292ad003
tranduythanh <coachtranduythanh@gmail.com>
2023-05-16 19:49:55 +0700

    done lab03

--- sol02.sql
+++ sol02.sql
@@ -1,3 +1,105 @@
+USE master;
+GO
+
+-- Set the database to single-user mode
+ALTER DATABASE Lab02_QLSX
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
+-- Drop the database
+DROP DATABASE Lab02_QLSX;
+GO
+
+----------ĐỊNH NGHĨA CƠ SỞ DỮ LIỆU----------------
+CREATE DATABASE Lab02_QLSX -- lenh khai bao CSDL
+
+go
+
+--lenh su dung CSDL
+use Lab02_QLSX
+
+go
+
+create table ToSanXuat
+(
+	MaTSX	char(4) primary key,
+	TenTSX	nvarchar(10) not null unique
+)
+
+go
+
+--lenh tao cac bang
+create table CongNhan
+(
+	MACN	 char(5) primary key,		
+    Ho       nvarchar(30) not null,
+    Ten      nvarchar(30) not null,
+    Phai     nvarchar(3) not null,
+    NgaySinh datetime,
+	MaTSX    char(4) NOT NULL references ToSanXuat(MaTSX)
+)
+
+go
+
+create table SanPham
+(
+	MASP	 char(5) primary key,
+	TenSP	 nvarchar(30) not null,
+	DVT      nvarchar(30) not null,
+    TienCong int not null check (TienCong > 0)
+)
+
+go
+
+create table ThanhPham
+(
+	MACN	char(5) not null references CongNhan(MACN),
+	MaSP    char(5) not null references SanPham(MaSP),
+    Ngay    datetime not null,
+    SoLuong int not null check (SoLuong > 0),
+    primary key (MACN, MaSP, Ngay)
+)
+
+set dateformat dmy
+go
+insert into ToSanXuat values('TS01', 'Tổ 1')
+insert into ToSanXuat values('TS02', 'Tổ 2')
+select * from ToSanXuat
+
+go
+
+set dateformat dmy
+insert into CongNhan values('CN001', N'Nguyễn Trường', N'An',    N'Nam', '12/05/1981', 'TS01')
+insert into CongNhan values('CN002', N'Lê Thị Hồng',   N'Gấm',   N'Nữ',  '04/06/1980', 'TS01')
+insert into CongNhan values('CN003', N'Nguyễn Công',   N'Thành', N'Nam', '04/05/1981', 'TS02')
+insert into CongNhan values('CN004', N'Võ Hữu',        N'Hạnh',  N'Nam', '15/02/1980', 'TS02')
+insert into CongNhan values('CN005', N'Lý Thanh',      N'Hân',   N'Nữ',  '03/12/1981', 'TS01')
+select * from CongNhan
+
+insert into SanPham values('SP001',N'Nồi Đất',      N'cái', 10000)
+insert into SanPham values('SP002',N'Chén',         N'cái', 2000)
+insert into SanPham values('SP003',N'Bình gốm nhỏ', N'cái', 20000)
+insert into SanPham values('SP004',N'Bình gốm lớn', N'cái', 25000)
+select * from SanPham
+
+set dateformat dmy
+go
+insert into ThanhPham values('CN001', 'SP001', '01/02/2007', 10)
+insert into ThanhPham values('CN002', 'SP001', '01/02/2007', 5)
+insert into ThanhPham values('CN003', 'SP002', '10/01/2007', 50)
+insert into ThanhPham values('CN004', 'SP003', '12/01/2007', 10)
+insert into ThanhPham values('CN005', 'SP002', '12/01/2007', 100)
+insert into ThanhPham values('CN002', 'SP004', '13/02/2007', 10)
+insert into ThanhPham values('CN001', 'SP003', '14/02/2007', 15)
+insert into ThanhPham values('CN003', 'SP001', '15/01/2007', 20)
+insert into ThanhPham values('CN003', 'SP004', '14/02/2007', 15)
+insert into ThanhPham values('CN004', 'SP002', '30/01/2007', 100)
+insert into ThanhPham values('CN005', 'SP003', '01/02/2007', 50)
+insert into ThanhPham values('CN001', 'SP001', '20/02/2007', 30)
+select * from ThanhPham
+----------------------------------------------------------------------
+
 -- 1
 SELECT
 	tsx.TenTSX,
--- sol03.sql
+++ sol03.sql
@@ -1,3 +1,151 @@
+USE master;
+GO
+
+-- Set the database to single-user mode
+ALTER DATABASE Lab03_QLNXHH
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
+-- Drop the database
+DROP DATABASE Lab03_QLNXHH;
+GO
+
+CREATE DATABASE Lab03_QLNXHH
+
+go
+use Lab03_QLNXHH
+
+
+CREATE TABLE HangHoa (
+    MAHH varchar(5) PRIMARY KEY,
+    TENHH varchar(40) NOT NULL,
+    DVT nvarchar(10),
+    SOLUONGTON int CHECK (SOLUONGTON >= 0)
+);
+
+
+CREATE TABLE DoiTac (
+    MADT char(5) PRIMARY KEY,
+    TENDT nvarchar(30) NOT NULL,
+    DIACHI nvarchar(40) NOT NULL,
+    DIENTHOAI varchar(12),
+    CONSTRAINT CHK_DIENTHOAI CHECK (DIENTHOAI LIKE '[0-9]%')
+);
+
+
+SET DATEFORMAT dmy;
+CREATE TABLE HoaDon (
+    SOHD char(5) PRIMARY KEY,
+    NGAYLAPHD datetime,
+    MADT char(5) REFERENCES DoiTac(MADT),
+    TONGTG int,
+    CONSTRAINT CHK_NGAYLAPHD CHECK (NGAYLAPHD <= GETDATE())
+);
+
+
+CREATE TABLE KhaNangCC (
+    MADT char(5) REFERENCES DoiTac(MADT),
+    MAHH varchar(5) REFERENCES HangHoa(MAHH),
+    PRIMARY KEY(MADT, MAHH)
+);
+
+
+CREATE TABLE CT_HoaDon (
+    SOHD char(5) REFERENCES HoaDon(SOHD),
+    MAHH varchar(5) REFERENCES HangHoa(MAHH),
+    DONGIA int,
+    SOLUONG int,
+    PRIMARY KEY (SOHD, MAHH),
+    CONSTRAINT CHK_DONGIA CHECK (DONGIA > 0),
+    CONSTRAINT CHK_SOLUONG CHECK (SOLUONG > 0)
+);
+
+-------------NHAP DU LIEU CHO CAC BANG-----------
+--Nhập dữ liệu cho bảng Hàng Hoá
+insert into HangHoa values('CPU01','CPU INTEL, CELERON 600 BOX',N'CÁI',5)
+insert into HangHoa values('CPU02','CPU INTEL, PIII 700',       N'CÁI',10)
+insert into HangHoa values('CPU03','CPU AMD K7 ATHL,ON 600',    N'CÁI',8)
+insert into HangHoa values('HDD01','HDD 10.2 GB QUANTUM',       N'CÁI',10)
+insert into HangHoa values('HDD02','HDD 13.6 GB SEAGATE',       N'CÁI',15)
+insert into HangHoa values('HDD03','HDD 20 GB QUANTUM',         N'CÁI',6)
+insert into HangHoa values('KB01', 'KB GENIUS',                 N'CÁI',12)
+insert into HangHoa values('KB02', 'KB MITSUMIMI',              N'CÁI',5)
+insert into HangHoa values('MB01', 'GIGABYTE CHIPSET INTEL',    N'CÁI',10)
+insert into HangHoa values('MB02', 'ACOPR BX CHIPSET VIA',      N'CÁI',10)
+insert into HangHoa values('MB03', 'INTEL PHI CHIPSET INTEL',   N'CÁI',10)
+insert into HangHoa values('MB04', 'ECS CHIPSET SIS',           N'CÁI',10)
+insert into HangHoa values('MB05', 'ECS CHIPSET VIA',           N'CÁI',10)
+insert into HangHoa values('MNT01','SAMSUNG 14" SYNCMASTER',    N'CÁI',5)
+insert into HangHoa values('MNT02','LG 14"',                    N'CÁI',5)
+insert into HangHoa values('MNT03','ACER 14"',                  N'CÁI',8)
+insert into HangHoa values('MNT04','PHILIPS 14"',               N'CÁI',6)
+insert into HangHoa values('MNT05','VIEWSONIC 14"',             N'CÁI',7)
+
+SELECT * FROM HangHoa
+--DELETE FROM HangHoa
+
+insert into DoiTac values('CC001',N'Cty TNC',N'176 BTX Q1 - TP.HCM',                          '08.8250259')
+insert into DoiTac values('CC002',N'Cty Hoàng Long',N'15A TTT Q1 - TP.HCM',                   '08.8250898')
+insert into DoiTac values('CC003',N'Cty Hợp Nhất',N'152 BTX Q1 - TP.HCM',                     '08.8252376')
+insert into DoiTac values('K0001',N'Nguyễn Minh Hải',N'91 Nguyễn Văn Trỗi Tp.Đà Lạt',         '063.831129')
+insert into DoiTac values('K0002',N'Như Quỳnh',N'21 Điện Biên Phủ. N.Trang',                  '058590270')
+insert into DoiTac values('K0003',N'Trần Nhật Duật',N'Lê Lợi TP.Huế',                         '054.848376')
+insert into DoiTac values('K0004',N'Phan Nguyễn Hùng Anh',N'11 Nam Kỳ Khởi Nghĩa - TP.Đà lạt','063.823409')
+
+SELECT * FROM DoiTac
+--DELETE FROM DoiTac
+
+set dateformat dmy 
+go
+insert into HoaDon values('N0001','25/01/2006','CC001',null)
+insert into HoaDon values('N0002','01/05/2006','CC002',null)
+insert into HoaDon values('X0001','12/05/2006','K0001',null)
+insert into HoaDon values('X0002','16/06/2006','K0002',null)
+insert into HoaDon values('X0003','20/04/2006','K0001',null)
+
+SELECT * FROM HoaDon
+
+insert into KhaNangCC values('CC001','CPU01')
+insert into KhaNangCC values('CC001','HDD03')
+insert into KhaNangCC values('CC001','KB01')
+insert into KhaNangCC values('CC001','MB02')
+insert into KhaNangCC values('CC001','MB04')
+insert into KhaNangCC values('CC001','MNT01')
+insert into KhaNangCC values('CC002','CPU01')
+insert into KhaNangCC values('CC002','CPU02')
+insert into KhaNangCC values('CC002','CPU03')
+insert into KhaNangCC values('CC002','KB02')
+insert into KhaNangCC values('CC002','MB01')
+insert into KhaNangCC values('CC002','MB05')
+insert into KhaNangCC values('CC002','MNT03')
+insert into KhaNangCC values('CC003','HDD01')
+insert into KhaNangCC values('CC003','HDD02')
+insert into KhaNangCC values('CC003','HDD03')
+insert into KhaNangCC values('CC003','MB03')
+
+SELECT * FROM KhaNangCC
+
+insert into CT_HoaDon values('N0001','CPU01',63,10)
+insert into CT_HoaDon values('N0001','HDD03',97,7)
+insert into CT_HoaDon values('N0001','KB01', 3,5)
+insert into CT_HoaDon values('N0001','MB02', 57,5)
+insert into CT_HoaDon values('N0001','MNT01',112,3)
+insert into CT_HoaDon values('N0002','CPU02',115,3)
+insert into CT_HoaDon values('N0002','KB02', 5,7)
+insert into CT_HoaDon values('N0002','MNT03',111,5)
+insert into CT_HoaDon values('X0001','CPU01',67,2)
+insert into CT_HoaDon values('X0001','HDD03',100,2)
+insert into CT_HoaDon values('X0001','KB01', 5,2)
+insert into CT_HoaDon values('X0001','MB02', 62,1)
+insert into CT_HoaDon values('X0002','CPU01',67,1)
+insert into CT_HoaDon values('X0002','KB02', 7,3)
+insert into CT_HoaDon values('X0002','MNT01',115,2)
+insert into CT_HoaDon values('X0003','CPU01',67,1)
+insert into CT_HoaDon values('X0003','MNT03',115,2)
+
+SELECT * FROM CT_HoaDon
+
 -- 1.
 SELECT * FROM HangHoa WHERE MAHH LIKE 'HDD%';
 
@@ -123,6 +271,7 @@ UPDATE HoaDon SET HoaDon.TONGTG = TongKet.TongTG FROM HoaDon, TongKet;
 
 -- A.a 
 IF OBJECT_ID('TongSoLuongNhap') IS NOT NULL DROP FUNCTION TongSoLuongNhap;
+GO
 CREATE FUNCTION TongSoLuongNhap(
 	@MAHH char(5),
 	@From datetime,
@@ -138,46 +287,52 @@ BEGIN
 		SOHD IN (SELECT SOHD FROM HoaDon WHERE NGAYLAPHD >= @From AND NGAYLAPHD <= @To)
 	RETURN @sum
 END;
+GO
 SELECT dbo.TongSoLuongNhap('CPU01', '2006/01/01', '2006/07/01');
 
 -- A.b 
 IF OBJECT_ID('TongSoLuongXuat') IS NOT NULL DROP FUNCTION TongSoLuongXuat;
+GO
 CREATE FUNCTION TongSoLuongXuat (
-	@MAHH char(5),
-	@From datetime,
-	@To   datetime
-)
-RETURNs INT
-AS
-BEGIN
-	DECLARE @sum INT;
-	SELECT @sum = SUM(SOLUONG) FROM CT_HoaDon 
-	WHERE 	MAHH = @MAHH AND 
-		LEFT(SOHD, 1) = 'X' AND
-		SOHD IN (SELECT SOHD FROM HoaDon WHERE NGAYLAPHD >= @From AND NGAYLAPHD <= @To)
-	RETURN @sum;
-END;
-SELECT dbo.TongSoLuongXuat('CPU01', '2006/01/01', '2006/07/01');
-
--- A.c 
-IF OBJECT_ID('TongDoanhThuThang') IS NOT NULL DROP FUNCTION TongDoanhThuThang;
-CREATE FUNCTION TongDoanhThuThang (
-	@Thang int,
-	@Nam   int
+    @MAHH char(5),
+    @From datetime,
+    @To   datetime
 )
 RETURNS INT
 AS
 BEGIN
-	DECLARE @sum INT;
-	SELECT @sum = SUM(DONGIA*SOLUONG) FROM CT_HoaDon 
-	WHERE 	LEFT(SOHD, 1)='X' AND 
-		SOHD IN (SELECT SOHD FROM HoaDon WHERE MONTH(NGAYLAPHD)=@Thang AND YEAR(NGAYLAPHD)=@Nam)
-	RETURN @sum;
+    DECLARE @sum INT;
+    SELECT @sum = SUM(SOLUONG) FROM CT_HoaDon 
+    WHERE  MAHH = @MAHH AND 
+        LEFT(SOHD, 1) = 'X' AND
+        SOHD IN (SELECT SOHD FROM HoaDon WHERE NGAYLAPHD >= @From AND NGAYLAPHD <= @To)
+    RETURN @sum;
 END;
+GO
+SELECT dbo.TongSoLuongXuat('CPU01', '2006/01/01', '2006/07/01');
+
+-- A.c 
+IF OBJECT_ID('TongDoanhThuThang') IS NOT NULL DROP FUNCTION TongDoanhThuThang;
+GO
+CREATE FUNCTION TongDoanhThuThang (
+    @Thang int,
+    @Nam   int
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @sum INT;
+    SELECT @sum = ISNULL(SUM(DONGIA*SOLUONG), 0) FROM CT_HoaDon 
+    WHERE   LEFT(SOHD, 1)='X' AND 
+        SOHD IN (SELECT SOHD FROM HoaDon WHERE MONTH(NGAYLAPHD)=@Thang AND YEAR(NGAYLAPHD)=@Nam)
+    RETURN @sum;
+END;
+GO
 SELECT dbo.TongDoanhThuThang(5, 2006);
 
 -- A.d 
 IF OBJECT_ID('TongDoanhThuHangHoa') IS NOT NULL DROP FUNCTION TongDoanhThuHangHoa;
+GO
 CREATE FUNCTION TongDoanhThuHangHoa(
 	@MAHH char(5),
 	@From datetime,
@@ -187,17 +342,18 @@ RETURNS INT
 AS
 BEGIN
 	DECLARE @sum INT;
-	SELECT @sum = SUM(DONGIA*SOLUONG) FROM CT_HoaDon 
+	SELECT @sum = ISNULL(SUM(DONGIA*SOLUONG), 0) FROM CT_HoaDon 
 	WHERE 	LEFT(SOHD, 1)='X' AND 
 		MAHH = @MAHH AND
 		SOHD IN (SELECT SOHD FROM HoaDon WHERE NGAYLAPHD >= @From AND NGAYLAPHD <= @To)
 	RETURN @sum;
 END;
-
+GO
 SELECT dbo.TongDoanhThuHangHoa('CPU01', '2006/01/01', '2006/07/01');
 
 -- A.e 
 IF OBJECT_ID('TongTienNhapHang') IS NOT NULL DROP FUNCTION TongTienNhapHang;
+GO
 CREATE FUNCTION TongTienNhapHang (
 	@From datetime,
 	@To   datetime
@@ -206,15 +362,17 @@ RETURNS INT
 AS
 BEGIN
 	DECLARE @sum INT;
-	SELECT @sum = SUM(DONGIA*SOLUONG) FROM CT_HoaDon 
+	SELECT @sum = ISNULL(SUM(DONGIA*SOLUONG), 0) FROM CT_HoaDon 
 	WHERE 	LEFT(SOHD, 1)='X' AND 
 		SOHD IN (SELECT SOHD FROM HoaDon WHERE NGAYLAPHD >= @From AND NGAYLAPHD <= @To)
 	RETURN @sum
 END;
+GO
 SELECT dbo.TongTienNhapHang('2006/01/01', '2006/07/01');
 
 -- A.f
 IF OBJECT_ID('TongTienHoaDon') IS NOT NULL DROP FUNCTION TongTienHoaDon;
+GO
 CREATE FUNCTION TongTienHoaDon (
 	@SOHD char(5)
 )
@@ -222,14 +380,16 @@ RETURNS INT
 AS
 BEGIN
 	DECLARE @sum INT;
-	SELECT @sum = SUM(DONGIA*SOLUONG) FROM CT_HoaDon 
+	SELECT @sum = ISNULL(SUM(DONGIA*SOLUONG), 0) FROM CT_HoaDon 
 	WHERE 	SOHD = @SOHD
 	RETURN @sum
 END;
+GO
 SELECT dbo.TongTienHoaDon('N0001');
 
 -- B.a 
 IF OBJECT_ID('CapNhatSoLuongTon') IS NOT NULL DROP PROCEDURE CapNhatSoLuongTon;
+GO
 CREATE PROCEDURE CapNhatSoLuongTon
 	@SOHD char(5)
 AS
@@ -240,22 +400,24 @@ WITH t AS (
 )
 UPDATE HangHoa SET SOLUONGTON = SOLUONGTON+t.Delta FROM t WHERE t.MAHH = HangHoa.MAHH;
 END;
-
+GO
 EXEC CapNhatSoLuongTon 'N0001';
 
 -- B.b
 IF OBJECT_ID('TinhTongTG') IS NOT NULL DROP PROCEDURE TinhTongTG;
+GO
 CREATE PROCEDURE TinhTongTG
 	@SOHD char(5)
 AS
 BEGIN
-UPDATE HoaDon SET TONGTG = (SELECT SUM(DONGIA*SOLUONG) FROM CT_HoaDon WHERE SOHD = @SOHD) WHERE SOHD = @SOHD
+UPDATE HoaDon SET TONGTG = (SELECT ISNULL(SUM(DONGIA*SOLUONG), 0) FROM CT_HoaDon WHERE SOHD = @SOHD) WHERE SOHD = @SOHD
 END;
-
+GO
 EXEC TinhTongTG 'N0001';
 
 -- B.c
 IF OBJECT_ID('XuatHoaDon') IS NOT NULL DROP PROCEDURE XuatHoaDon;
+GO
 CREATE PROCEDURE XuatHoaDon
 	@SOHD char(5)
 AS
@@ -269,6 +431,7 @@ SELECT
 	ct.SOLUONG
 FROM HoaDon AS hd
 JOIN CT_HoaDon AS ct ON hd.SOHD = ct.SOHD
-WHERE ct.SOHD = 'N0001'
+WHERE ct.SOHD = @SOHD
 END;
+GO
 EXEC XuatHoaDon 'N0001';
